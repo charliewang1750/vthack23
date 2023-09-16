d0e95d289249f11632c3db706021810454f4d7f2
realcharliewang <124309275+realcharliewang@users.noreply.github.com>
2023-09-16 19:19:29 -0400

    something

---
+++ src/main/java/com/zillow/vthack23/Amazon.java
@@ -0,0 +1,20 @@
+package com.zillow.vthack23;
+import java.util.ArrayList;
+
+public class Amazon {
+    private ArrayList<Product> stuff = new ArrayList<Product>();
+
+
+    public ArrayList<Product> printList(){
+        //prints stuff
+        return stuff;
+    }
+
+    public void addRandom(){
+        //adds random product to stuff
+        Product newProduct = new Product();
+        stuff.add(newProduct);
+        return;
+    }
+
+}
