63c89ed2db3c10b054cc56c66981e351b4ca6168
realcharliewang <124309275+realcharliewang@users.noreply.github.com>
2023-09-16 17:19:44 -0400

    New repository

---
+++ src/main/java/com/zillow/vthack23/Vthack23Application.java
@@ -0,0 +1,13 @@
+package com.zillow.vthack23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vthack23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vthack23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zillow/vthack23/Vthack23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zillow.vthack23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vthack23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
