00828b58306a9cba8ec76dba8f8d6745ca34d71e
realcharliewang <124309275+realcharliewang@users.noreply.github.com>
2023-09-16 17:22:19 -0400

    RealEstate + controller

---
+++ src/main/java/com/zillow/vthack23/RealEstate.java
@@ -0,0 +1,5 @@
+package com.zillow.vthack23;
+
+public class RealEstate {
+    
+}
---
+++ src/main/java/com/zillow/vthack23/RealEstateController.java
@@ -0,0 +1,13 @@
+package com.zillow.vthack23;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RealEstateController {
+    
+    @RequestMapping("/houses")
+    public String hello(){
+        return "hello";
+    }
+}
